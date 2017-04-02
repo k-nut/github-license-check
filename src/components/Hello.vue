20459c9cf9f7e2ccce96d469cc41ea7e7eb84023
Knut Hühne <knut@k-nut.eu>
2017-04-02 13:38:03 +0200

    extract api code
    this should make it easier to test. If I could
    figure out how testing works with vue

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -48,52 +48,7 @@
 
 <script>
 import _ from 'lodash'
-
-function status (response) {
-  if (response.status >= 200 && response.status < 300) {
-    return Promise.resolve(response)
-  } else {
-    return Promise.reject(new Error(response.statusText))
-  }
-}
-
-function json (response) {
-  return response.json()
-}
-
-function extractData (repos) {
-  return repos.map(function (r) {
-    return { name: r.name, license: r.license, fork: r.fork, url: r.html_url }
-  })
-}
-
-function getRepos (name, page = 1) {
-  const request = new Request(`https://api.github.com/users/${name}/repos?per_page=100&page=${page}`, {
-    headers: new Headers({
-      'Accept': 'application/vnd.github.drax-preview+json'
-    }),
-    mode: 'cors'
-  })
-
-  return fetch(request)
-    .then(status)
-    .then(json)
-    .then(extractData)
-    .catch(function (error) {
-      console.log('Request failed', error)
-    })
-}
-
-function getAllRepos (userName, page = 1, allRepos = []) {
-  return getRepos(userName, page).then(repos => {
-    allRepos = allRepos.concat(repos)
-    if (repos.length === 100) {
-      return getAllRepos(userName, page + 1, allRepos)
-    } else {
-      return allRepos
-    }
-  })
-}
+import { getAllRepos } from '../api'
 
 export default {
   data () {
